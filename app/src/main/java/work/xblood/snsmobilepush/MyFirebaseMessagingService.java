8c4af885a7b9d91a21a67b43e603644e53185c8e
x-blood <yasaisato@gmail.com>
2020-01-03 23:40:42 +0900

    [change] Add output log process of Body and Title payload.

--- app/src/main/java/work/xblood/snsmobilepush/MyFirebaseMessagingService.java
+++ app/src/main/java/work/xblood/snsmobilepush/MyFirebaseMessagingService.java
@@ -25,5 +25,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
         }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            Log.d(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
+        }
     }
 }
